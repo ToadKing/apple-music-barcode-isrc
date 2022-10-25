0f43fbd6d70e71fa3f031dd726c8f6b7d1041d81
Michael Lelli <michael.lelli@steelseries.com>
2022-10-25 17:19:57 -0500

    update script to work with new site
    switch to fetching the album data from the backend

--- apple-music-barcode-isrc.user.js
+++ apple-music-barcode-isrc.user.js
@@ -2,12 +2,30 @@
 // @name        Apple Music Barcodes/ISRCs
 // @namespace   applemusic.barcode.isrc
 // @description Get Barcodes/ISRCs/etc. from Apple Music pages
-// @version     0.11
+// @version     0.12
 // @grant       none
 // @include     https://music.apple.com/*
 // @grant       none
 // @run-at      document-idle
+// @grant       GM_xmlhttpRequest
 // ==/UserScript==
+
+// TODO: try to find this token programmatically? It's hardcoded into scripts now so maybe not practical.
+const token = 'eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IldlYlBsYXlLaWQifQ.eyJpc3MiOiJBTVBXZWJQbGF5IiwiaWF0IjoxNjY2MjA5MTY4LCJleHAiOjE2NzM0NjY3NjgsInJvb3RfaHR0cHNfb3JpZ2luIjpbImFwcGxlLmNvbSJdfQ.txDAv_8oy4G8zaens4enLYtADkP_zwcVLfT4NKW9l6Bt9KEyaYj566gtchMPJ-5cj7xxxrFsbd6wJcPbNvBRCg'
+const baseURL = 'https://amp-api.music.apple.com/v1'
+
+// Needs to use GM_xmlhttpRequest
+// 1. we need to set the origin header to any value, which you normally cannot do with fetch, because
+// 2. we may be running in the content script context instead of the page one if we're in Firefox
+function asyncGM_xmlhttpRequest(req) {
+  return new Promise((resolve, reject) => {
+    GM_xmlhttpRequest({
+      ...req,
+      onload(e) { resolve(e.response) },
+      onerror(e) { reject(e) },
+    })
+  })
+}
 
 function addSimple(content, node, parent) {
   const elem = document.createElement(node)
@@ -17,68 +35,9 @@ function addSimple(content, node, parent) {
   return elem
 }
 
-function getDatums() {
+async function getDatums() {
   try {
-    const music_info = JSON.parse(document.getElementById('shoebox-media-api-cache-amp-music').innerHTML)
-
-    const albums = []
-
-    for (const [key, value] of Object.entries(music_info).filter(([key]) => key.startsWith('\uF8FF.catalog.'))) {
-      const albumsData = JSON.parse(value)
-
-      for (const albumData of albumsData.d.filter((item) => item.type === 'albums')) {
-        const album = {
-          name: albumData.attributes.name,
-          artist: albumData.attributes.artistName,
-          releaseDate: albumData.attributes.releaseDate,
-          label: albumData.attributes.recordLabel,
-          barcode: albumData.attributes.upc,
-          isMasteredForItunes: albumData.attributes.isMasteredForItunes,
-          audio: albumData.attributes.audioTraits,
-          copyright: albumData.attributes.copyright,
-          tracks: [],
-          differentDates: false,
-        }
-
-        if (albumData.relationships.tracks) {
-          let tracksHaveDates = false
-          for (const track_data of albumData.relationships.tracks.data) {
-            const track = {
-              name: track_data.attributes.name,
-              artist: track_data.attributes.artistName,
-              composer: track_data.attributes.composerName,
-              disc: track_data.attributes.discNumber,
-              track: track_data.attributes.trackNumber,
-              isrc: track_data.attributes.isrc,
-              releaseDate: track_data.attributes.releaseDate,
-            }
-
-            if (track.releaseDate !== album.releaseDate) {
-              album.differentDates = true
-            }
-
-            if (!!track_data.attributes.releaseDate) {
-              tracksHaveDates = true
-            }
-
-            album.tracks.push(track)
-          }
-
-          if (!tracksHaveDates) {
-            // no tracks have release dates, unset the different dates flag
-            album.differentDates = false
-          }
-        }
-
-        albums.push(album)
-      }
-    }
-
-    if (albums.length === 0) {
-      throw new Error('no albums found')
-    }
-
-    const results = addSimple('', 'div', document.body)
+    const results = addSimple('Loading, press ESC to close...', 'div', document.body)
     results.style.position = 'absolute'
     results.style.inset = '30px'
     results.style.zIndex = 2147483647
@@ -86,6 +45,77 @@ function getDatums() {
     results.style.color = 'black'
     results.style.overflow = 'auto'
     results.style.padding = '4px'
+
+    const close = () => {
+      document.body.removeEventListener('keydown', close)
+      results.remove()
+    }
+    document.body.addEventListener('keydown', (e) => { if (e.key === 'Escape') close() })
+
+    const albumId = document.location.pathname.split('/').reverse().find(p => /^\d+$/.test(p))
+    const country = document.location.pathname.split('/')[1]
+    const url = `${baseURL}/catalog/${country}/albums/${albumId}`
+
+    const res = await asyncGM_xmlhttpRequest({ url, method: 'GET', mode: 'cors', credentials: 'include', headers: { Authorization: `Bearer ${token}`, Origin: new URL(baseURL).origin } })
+    const resJson = JSON.parse(res)
+    console.log(resJson)
+    const albumsData = resJson.data
+
+    const albums = []
+
+    for (const albumData of albumsData.filter((item) => item.type === 'albums')) {
+      const album = {
+        name: albumData.attributes.name,
+        artist: albumData.attributes.artistName,
+        releaseDate: albumData.attributes.releaseDate,
+        label: albumData.attributes.recordLabel,
+        barcode: albumData.attributes.upc,
+        isMasteredForItunes: albumData.attributes.isMasteredForItunes,
+        audio: albumData.attributes.audioTraits,
+        copyright: albumData.attributes.copyright,
+        tracks: [],
+        differentDates: false,
+      }
+
+      if (albumData.relationships.tracks) {
+        let tracksHaveDates = false
+        for (const track_data of albumData.relationships.tracks.data) {
+          const track = {
+            name: track_data.attributes.name,
+            artist: track_data.attributes.artistName,
+            composer: track_data.attributes.composerName,
+            disc: track_data.attributes.discNumber,
+            track: track_data.attributes.trackNumber,
+            isrc: track_data.attributes.isrc,
+            releaseDate: track_data.attributes.releaseDate,
+          }
+
+          if (track.releaseDate !== album.releaseDate) {
+            album.differentDates = true
+          }
+
+          if (!!track_data.attributes.releaseDate) {
+            tracksHaveDates = true
+          }
+
+          album.tracks.push(track)
+        }
+
+        if (!tracksHaveDates) {
+          // no tracks have release dates, unset the different dates flag
+          album.differentDates = false
+        }
+      }
+
+      albums.push(album)
+    }
+
+    if (albums.length === 0) {
+      close()
+      throw new Error('no albums found')
+    }
+
+    results.textContent = ''
 
     for (const album of albums) {
       addSimple(album.name, 'h1', results)
@@ -108,6 +138,8 @@ function getDatums() {
       kepstinLink.target = '_blank'
       // we intentionally don't use the `isrcM-T` format due to differences in how Apple Music formats some track lists
       kepstinLink.href = 'https://magicisrc.kepstin.ca/?' + album.tracks.map((track, i) => `isrc${i+1}=${track.isrc}`).join('&')
+      kepstinLink.style.color = '#06c'
+      kepstinLink.style.textDecoration = 'underline'
       // work around Apple Music's handling of links
       kepstinLink.addEventListener('click', e => {
         e.preventDefault()
@@ -171,13 +203,7 @@ function getDatums() {
       }
     }
 
-    const close = (e) => {
-      if (e.key === 'Escape') {
-        document.body.removeEventListener('keydown', close)
-        results.remove()
-      }
-    }
-    document.body.addEventListener('keydown', close)
+    addSimple('Press ESC to close', 'p', results)
   } catch (e) {
     alert(e)
   }
